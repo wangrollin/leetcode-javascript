c7a101faac415e7a41eda145b011d97e5f674b1a
wangrollin <wangrollin@foxmail.com>
2020-10-16 17:37:06 +0800

    第217题 存在重复元素

---
+++ src/com/wangrollin/leetcode_js/n0_normal/p200/p210/problem217/Solution2.js
@@ -0,0 +1,34 @@
+/**
+ * 存在重复元素
+ *
+ * 给定一个整数数组，判断是否存在重复元素。
+ * 如果任何值在数组中出现至少两次，函数返回 true。如果数组中每个元素都不相同，则返回 false。
+ *
+ * 示例 1:
+ * 输入: [1,2,3,1]
+ * 输出: true
+ *
+ * 示例 2:
+ * 输入: [1,2,3,4]
+ * 输出: false
+ *
+ * 示例3:
+ * 输入: [1,1,1,3,3,4,3,2,4,2]
+ * 输出: true
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/contains-duplicate
+ *
+ * Solution2
+ * [...Set]
+ */
+
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+let containsDuplicate = function (nums) {
+
+    let uniqueNums = [...new Set(nums)];
+    return uniqueNums.length !== nums.length;
+};
