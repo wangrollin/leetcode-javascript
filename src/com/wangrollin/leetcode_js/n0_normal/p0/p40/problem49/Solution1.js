b0c17867211f9696dd6a47b1c1e74a968c34f35f
wangrollin <wangrollin@foxmail.com>
2020-10-16 17:27:10 +0800

    第49题 字母异位词分组

---
+++ src/com/wangrollin/leetcode_js/n0_normal/p0/p40/problem49/Solution1.js
@@ -0,0 +1,48 @@
+/**
+ * 字母异位词分组
+ *
+ * 给定一个字符串数组，将字母异位词组合在一起。字母异位词指字母相同，但排列不同的字符串。
+ *
+ * 示例:
+ * 输入: ["eat", "tea", "tan", "ate", "nat", "bat"]
+ * 输出:
+ * [
+ *   ["ate","eat","tea"],
+ *   ["nat","tan"],
+ *   ["bat"]
+ * ]
+ *
+ * 说明：
+ * 所有输入均为小写字母。
+ * 不考虑答案输出的顺序。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/group-anagrams
+ *
+ * Solution1
+ * 使用排序后的str作为key，存储list，最后汇总
+ */
+
+/**
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
+let groupAnagrams = function (strs) {
+
+    let resultMap = new Map();
+
+    strs.forEach(word => {
+
+        let key = word.split("").sort().join("");
+
+        if (resultMap.has(key)) {
+            resultMap.get(key).push(word);
+        } else {
+            resultMap.set(key, [word]);
+        }
+    });
+
+    let result = [];
+    resultMap.forEach(value => result.push(value));
+    return result;
+};
---
+++ src/com/wangrollin/leetcode_js/n0_normal/p0/p40/problem49/Solution2.js
@@ -0,0 +1,58 @@
+/**
+ * 字母异位词分组
+ *
+ * 给定一个字符串数组，将字母异位词组合在一起。字母异位词指字母相同，但排列不同的字符串。
+ *
+ * 示例:
+ * 输入: ["eat", "tea", "tan", "ate", "nat", "bat"]
+ * 输出:
+ * [
+ *   ["ate","eat","tea"],
+ *   ["nat","tan"],
+ *   ["bat"]
+ * ]
+ *
+ * 说明：
+ * 所有输入均为小写字母。
+ * 不考虑答案输出的顺序。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/group-anagrams
+ *
+ * Solution2
+ * 使用letter count map作为key，存储list，最后汇总
+ */
+
+/**
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
+let groupAnagrams = function (strs) {
+
+    let resultMap = new Map();
+
+    strs.forEach(word => {
+
+        let keyMap = new Map();
+
+        word.split("").forEach(value => {
+            if (keyMap.has(value)) {
+                keyMap.set(value, keyMap.get(value) + 1);
+            } else {
+                keyMap.set(value, 1);
+            }
+        });
+
+        let keyMapStr = JSON.stringify([...keyMap].sort());
+
+        if (resultMap.has(keyMapStr)) {
+            resultMap.get(keyMapStr).push(word);
+        } else {
+            resultMap.set(keyMapStr, [word]);
+        }
+    });
+
+    let result = [];
+    resultMap.forEach(value => result.push(value));
+    return result;
+}
