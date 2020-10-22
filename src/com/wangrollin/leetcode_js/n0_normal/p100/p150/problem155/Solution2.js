5643e4af0ffc44fdb60923fba5c3b8263f5ace68
wangrollin <wangrollin@foxmail.com>
2020-10-22 11:07:23 +0800

    第155题 最小栈

---
+++ src/com/wangrollin/leetcode_js/n0_normal/p100/p150/problem155/Solution2.js
@@ -0,0 +1,84 @@
+/**
+ * 最小栈
+ * 设计一个支持 push，pop，top 操作，并能在常数时间内检索到最小元素的栈。
+ * push(x)-- 将元素 x 推入栈中。
+ * pop()-- 删除栈顶的元素。
+ * top()-- 获取栈顶元素。
+ * getMin() -- 检索栈中的最小元素。
+ *
+ * 示例:
+ * MinStack minStack = new MinStack();
+ * minStack.push(-2);
+ * minStack.push(0);
+ * minStack.push(-3);
+ * minStack.getMin();   --> 返回 -3.
+ * minStack.pop();
+ * minStack.top();      --> 返回 0.
+ * minStack.getMin();   --> 返回 -2.
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/min-stack
+ *
+ * Solution2
+ * 维护一个不同步的队列
+ */
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * var obj = new MinStack()
+ * obj.push(x)
+ * obj.pop()
+ * var param_3 = obj.top()
+ * var param_4 = obj.getMin()
+ */
+class MinStack {
+
+    /**
+     * initialize your data structure here.
+     */
+    constructor() {
+        this.stack = [];
+        this.minValueStack = [];
+    }
+
+    /**
+     * @param {number} x
+     * @return {void}
+     */
+    push(x) {
+
+        if (this.minValueStack.length === 0) {
+            this.stack.push(x);
+            this.minValueStack.push(x);
+        } else {
+            this.stack.push(x);
+            if (x <= this.minValueStack[this.minValueStack.length - 1]) {
+                this.minValueStack.push(x);
+            }
+        }
+    }
+
+    /**
+     * @return {void}
+     */
+    pop() {
+        let value = this.stack.pop();
+        if (value === this.minValueStack[this.minValueStack.length - 1]) {
+            this.minValueStack.pop();
+        }
+    }
+
+    /**
+     * @return {number}
+     */
+    top() {
+        return this.stack[this.stack.length - 1];
+    }
+
+    /**
+     * @return {number}
+     */
+    getMin() {
+        return this.minValueStack[this.minValueStack.length - 1];
+    }
+}
