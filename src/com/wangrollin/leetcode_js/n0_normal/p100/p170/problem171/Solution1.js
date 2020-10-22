7909d3ce1db3f16e4a2de189ac7c85c906d2a6e3
wangrollin <wangrollin@foxmail.com>
2020-10-22 14:35:39 +0800

    第171题 Excel表列序号

---
+++ src/com/wangrollin/leetcode_js/n0_normal/p100/p170/problem171/Solution1.js
@@ -0,0 +1,50 @@
+/**
+ * Excel表列序号
+ *
+ * 给定一个Excel表格中的列名称，返回其相应的列序号。
+ *
+ * 例如，
+ *     A -> 1
+ *     B -> 2
+ *     C -> 3
+ *     ...
+ *     Z -> 26
+ *     AA -> 27
+ *     AB -> 28
+ *     ...
+ *
+ * 示例 1:
+ * 输入: "A"
+ * 输出: 1
+ *
+ * 示例2:
+ * 输入: "AB"
+ * 输出: 28
+ *
+ * 示例3:
+ * 输入: "ZY"
+ * 输出: 701
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/excel-sheet-column-number
+ *
+ * Solution1
+ * 进制转换问题
+ */
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+let titleToNumber = function (s) {
+
+    let result = 0;
+    for (let i = 0; i < s.length; i++) {
+        let tmp = s.charCodeAt(i) - 64;
+        for (let j = 0; j < s.length - 1 - i; j++) {
+            tmp *= 26;
+        }
+        result += tmp;
+    }
+    return result;
+};
