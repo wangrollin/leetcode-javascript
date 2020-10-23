ad23bab2ba4d2742606f6a6cacee4e8166bd311f
wangrollin <wangrollin@foxmail.com>
2020-10-23 13:52:01 +0800

    第504题 七进制数

---
+++ src/com/wangrollin/leetcode_js/n0_normal/p500/p500/problem504/Solution1.js
@@ -0,0 +1,47 @@
+/**
+ * 七进制数
+ *
+ * 给定一个整数，将其转化为7进制，并以字符串形式输出。
+ *
+ * 示例 1:
+ * 输入: 100
+ * 输出: "202"
+ *
+ * 示例 2:
+ * 输入: -7
+ * 输出: "-10"
+ *
+ * 注意: 输入范围是[-1e7, 1e7] 。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/base-7
+ *
+ * Solution1
+ * 取余
+ */
+
+/**
+ * @param {number} num
+ * @return {string}
+ */
+let convertToBase7 = function (num) {
+
+    if (num === 0) {
+        return "0";
+    }
+
+    let pre = "";
+    if (num < 0) {
+        pre = "-";
+        num *= -1;
+    }
+
+    let sb = [];
+    while (num !== 0) {
+        sb.push(num % 7);
+        num = Math.floor(num / 7);
+    }
+
+    sb.push(pre);
+    return sb.reverse().join("");
+};
